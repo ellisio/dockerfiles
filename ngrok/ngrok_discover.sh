198392c90aabff44f6276a00154eb7c767fb3169
Andrew Ellis <awellis89@gmail.com>
2017-03-17 15:13:49 -0600

    fixed ngrok for the new sweet way to do things

--- ngrok/ngrok_discover.sh
+++ ngrok/ngrok_discover.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
-if [ "$1" = "/bin/sh" ]; then
+if [ "$1" = "/bin/bash" ]; then
   shift
 fi
 
@@ -19,21 +19,22 @@ if [ -z "$NGROK_TOKEN" ]; then
   exit
 fi
 
-sed -i "s/^authtoken:.*/authtoken: $NGROK_TOKEN/g" /ngrok.yml
-ARGS="-config /ngrok.yml"
-
+ARGS=""
 IFS=','; for TUNNEL in `echo "$NGROK_TUNNELS"`; do
   cp /ngrok-tunnel.yml /ngrok-$TUNNEL.yml
 
   SUBDOMAIN=$NGROK_SUBDOMAIN
 
-  if [[ $TUNNEL != 'app' ]]; then
+  if [[ "$TUNNEL" != "app" ]]; then
     SUBDOMAIN="$SUBDOMAIN-$TUNNEL"
   fi
 
-  sed -i "s/##SUBDOMAIN##/$SUBDOMAIN" /ngrok-$app.yml
+  sed -i "s/##SUBDOMAIN##/$SUBDOMAIN/g" /ngrok-$TUNNEL.yml
 
-  ARGS="$ARGS -config=/ngrok-$app.yml"
+  ARGS="$ARGS -config /ngrok-$TUNNEL.yml"
 done
 
-exec /bin/ngrok $ARGS
+sed -i "s/^authtoken:.*/authtoken: $NGROK_TOKEN/g" /ngrok.yml
+ARGS="$ARGS -config /ngrok.yml"
+
+exec /bin/bash -c "/bin/ngrok start $ARGS --all"
